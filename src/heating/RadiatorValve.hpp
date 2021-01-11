11445342941573aa1a02af30b5e9ce011a009403
Alexander Mohr <alexander.m.mohr@daimler.com>
2021-01-11 23:20:42 +0100

    Changed regulator and add mode support to mqtt and web ui

--- src/heating/RadiatorValve.hpp
+++ src/heating/RadiatorValve.hpp
@@ -11,39 +11,43 @@
 #include <chrono>
 namespace open_heat {
 namespace heating {
+
 class RadiatorValve {
   public:
-  explicit RadiatorValve(
-    sensors::ITemperatureSensor& tempSensor,
-    Filesystem& filesystem);
+  enum Mode { HEAT, OFF, UNKNOWN };
+
+
+  RadiatorValve(sensors::ITemperatureSensor& tempSensor, Filesystem& filesystem);
 
   void loop();
   void setup();
   float getConfiguredTemp() const;
   void setConfiguredTemp(float temp);
+  void setMode(Mode mode);
+  Mode getMode();
+  static const char* modeToCharArray(const Mode mode);
 
   private:
-  static void rotateValve(short rotateTime);
   static void openValve(unsigned short rotateTime);
   static void closeValve(unsigned short rotateTime);
   static void setPinsLow();
-    void updateConfig();
+  void updateConfig();
 
   Filesystem& filesystem_;
+  Mode mode_;
 
   static constexpr int VALVE_COMPLETE_CLOSE_MILLIS = 5000;
   sensors::ITemperatureSensor& tempSensor_;
   float setTemp_;
-  float lastDiff_ = 0;
   float lastTemp_ = 0;
+  float lastPredictTemp_;
 
   static constexpr float tempMaxDiff_ = 0.1;
-  static constexpr float tempMinDiff_ = 0.1;
+  static constexpr float tempMinDiff_ = 0.4;
   bool turnOff_ = false;
 
   unsigned long nextCheckMillis_{0};
-  unsigned long checkIntervalMillis_ = 5 * 60 * 1000;
-
+  unsigned long checkIntervalMillis_ = 2 * 60 * 1000;
 };
 } // namespace heating
 } // namespace open_heat
--- src/network/WebServer.cpp
+++ src/network/WebServer.cpp
@@ -11,7 +11,7 @@ namespace open_heat {
 namespace network {
 
 static constexpr char HTML_INDEX[] PROGMEM
-  = R"(<html lang='en'><head><title>Open Heat</title><meta charset='utf-8'><meta name="viewport" content="width=device-width, user-scalable=no"> <script>if(!!window.EventSource){const logEvents=new EventSource('/logEvents');logEvents.addEventListener('message',function(e){const div=document.getElementById('logDiv');const paragraph=document.createElement("p");const text=document.createTextNode(e.data);const str=JSON.stringify(e.data);if(str.includes("[DEBUG]")||str.includes("[TRACE]")){paragraph.className="logDebug";}else if(str.includes("[INFO]")){paragraph.className="logInfo";}else{paragraph.className="logError";} paragraph.appendChild(text);div.insertBefore(paragraph,div.firstChild);},false);}</script> <style>html{background-color:#212121}p{font-weight:500}a{text-decoration:none}*{margin:0;padding:0;color:#E0E0E0;overflow-x:hidden}body{font-size:16px;font-family:'Roboto',sans-serif;font-weight:300;color:#4a4a4a}input{width:120px;background:#121212;border:none;border-radius:4px;padding:1rem;height:50px;margin:0.25em;font-size:1rem;box-shadow:0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0,0,0,0.23)}.input-group-text{width:120px;background:#121212;border:none;border-radius:4px;padding:1rem;height:50px;margin-left:-0.5em;z-index:-1;font-size:1rem;box-shadow:0 10px 20px -20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0,0,0,0.23)}.inputMedium{width:155px}.inputSmall{width:85px}.inputLarge{width:260px}label{margin-right:1em;font-size:1.15rem;display:inline-block;width:85px}.break{flex-basis:100%%;height:0}.btn{background:#303F9F;color:#EEE;border-radius:4px}.btnLarge{width:262px}.flex-container{display:flex;flex-wrap:wrap}.flex-nav{flex-grow:1;flex-shrink:0;background:#303F9F;height:3rem}.flex-menu{padding:1rem 2rem;float:right}.featured{background:#3F51B5;color:#fff;padding:1em}.featured h1{font-size:2rem;margin-bottom:1rem;font-weight:300}.flex-card{overflow-y:hidden;flex:1;flex-shrink:0;flex-basis:400px;display:flex;flex-wrap:wrap;background:#212121;margin: .5rem;box-shadow:0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23)}.flex-card div{flex:100%%}.flex-card .hero{position:relative;color:#fff;height:70px;background:linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)) no-repeat;background-size:cover}.flex-card .hero h3{position:absolute;bottom:15px;left:0;padding:0 1rem}.content{min-height:100%%;min-width:400px}.flex-card .content{color:#BDBDBD;padding:1.5rem 1rem 2rem 1rem}.logInfo{color:#2E7D32}.logDebug{color:#757575}.logError{color:#b71c1c}</style></head><body><div class="flex-container"><div class="flex-nav"></div></div><div class="featured"><h1><a href="/">Open Heat</a></h1></div><div class="flex-container animated zoomIn"><div class="flex-card"><div class="hero"><h3>Temperature</h3></div><div class="content"> <label for="currentTemp">Measured</label> <input class="inputSmall" readonly="readonly" id="currentTemp" value="%CURRENT_TEMP%"> <span class="input-group-text">°C</span><br><form method='POST' action='/' enctype='multipart/form-data'> <label for="setTemp">Set</label> <input class="inputSmall" id="setTemp" name="setTemp" type="number" value="%SET_TEMP%"> <span class="input-group-text">°C</span> <input type='submit' value='Confirm' class="btn" ></form><form method='POST' action='/off' enctype='multipart/form-data'> <label></label> <input type='submit' value='Turn off' class="btn btnLarge" ></form></div></div><div class="flex-card"><div class="hero"><h3>Settings</h3></div><div class="content"><h3>Network</h3><form method='POST' action='/' enctype='multipart/form-data'> <label for="netHost">Hostname</label> <input id="netHost" class="inputLarge" name="netHost" value="%HOSTNAME%"><br><h3>MQTT</h3> <label for="mqttHost">Host</label> <input id="mqttHost" class="inputMedium" name="mqttHost" value="%MQTT_HOST%"> : <input size="3" id="mqttPort" class="inputSmall" name="mqttPort" value="%MQTT_PORT%"><br><label for="mqttTopic">Topic</label> <input id="mqttTopic" class="inputLarge" name="mqttTopic" value="%MQTT_TOPIC%"><br><label for="mqttUsername">Username</label> <input id="mqttUsername" class="inputLarge" name="mqttUsername" value="%MQTT_USER%"><br><label for="mqttPassword">Password</label> <input id="mqttPassword" class="inputLarge" name="mqttPassword" value="%MQTT_PW%"><br> <br> <input type='submit' value='Update settings' class="btn btnLarge" ></form></div></div><div class="flex-card"><div class="hero"><h3>System</h3></div><div class="content"><h3>Firmware update</h3><form method='POST' action='/installUpdate' enctype='multipart/form-data'> <input type='file' class="input inputLarge" accept='.bin,.bin.gz' name='firmware' > <input type='submit' value='Update' class="btn" ></form> <br></div></div><div class="break"></div><div class="flex-card"><div class="hero"><h3>Log</h3></div><div class="content" id="logDiv"></div></div></div></body></html>)";
+  = R"(<html lang='en'><head><title>Open Heat</title><meta charset='utf-8'><meta name="viewport" content="width=device-width, user-scalable=no"> <script>if(!!window.EventSource){const logEvents=new EventSource('/logEvents');logEvents.addEventListener('message',function(e){const div=document.getElementById('logDiv');const paragraph=document.createElement("p");const text=document.createTextNode(e.data);const str=JSON.stringify(e.data);if(str.includes("[DEBUG]")||str.includes("[TRACE]")){paragraph.className="logDebug";}else if(str.includes("[INFO]")){paragraph.className="logInfo";}else{paragraph.className="logError";} paragraph.appendChild(text);div.insertBefore(paragraph,div.firstChild);},false);}</script> <style>html{background-color:#212121}p{font-weight:500}a{text-decoration:none}*{margin:0;padding:0;color:#E0E0E0;overflow-x:hidden}body{font-size:16px;font-family:'Roboto',sans-serif;font-weight:300;color:#4a4a4a}input{width:120px;background:#121212;border:none;border-radius:4px;padding:1rem;height:50px;margin:0.25em;font-size:1rem;box-shadow:0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0,0,0,0.23)}.input-group-text{width:120px;background:#121212;border:none;border-radius:4px;padding:1rem;height:50px;margin-left:-0.5em;z-index:-1;font-size:1rem;box-shadow:0 10px 20px -20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0,0,0,0.23)}.inputMedium{width:155px}.inputSmall{width:85px}.inputLarge{width:260px}label{margin-right:1em;font-size:1.15rem;display:inline-block;width:85px}.break{flex-basis:100%%;height:0}.btn{background:#303F9F;color:#EEE;border-radius:4px}.btnLarge{width:262px}.flex-container{display:flex;flex-wrap:wrap}.flex-nav{flex-grow:1;flex-shrink:0;background:#303F9F;height:3rem}.flex-menu{padding:1rem 2rem;float:right}.featured{background:#3F51B5;color:#fff;padding:1em}.featured h1{font-size:2rem;margin-bottom:1rem;font-weight:300}.flex-card{overflow-y:hidden;flex:1;flex-shrink:0;flex-basis:400px;display:flex;flex-wrap:wrap;background:#212121;margin: .5rem;box-shadow:0 10px 20px rgba(0, 0, 0, 0.19), 0 6px 6px rgba(0, 0, 0, 0.23)}.flex-card div{flex:100%%}.flex-card .hero{position:relative;color:#fff;height:70px;background:linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)) no-repeat;background-size:cover}.flex-card .hero h3{position:absolute;bottom:15px;left:0;padding:0 1rem}.content{min-height:100%%;min-width:400px}.flex-card .content{color:#BDBDBD;padding:1.5rem 1rem 2rem 1rem}.logInfo{color:#2E7D32}.logDebug{color:#757575}.logError{color:#b71c1c}</style></head><body><div class="flex-container"><div class="flex-nav"></div></div><div class="featured"><h1><a href="/">Open Heat</a></h1></div><div class="flex-container animated zoomIn"><div class="flex-card"><div class="hero"><h3>Temperature</h3></div><div class="content"> <label for="currentTemp">Measured</label> <input class="inputSmall" readonly="readonly" id="currentTemp" value="%CURRENT_TEMP%"> <span class="input-group-text">°C</span><br><form method='POST' action='/' enctype='multipart/form-data'> <label for="setTemp">Set</label> <input class="inputSmall" id="setTemp" name="setTemp" type="number" value="%SET_TEMP%"> <span class="input-group-text">°C</span> <input type='submit' value='Confirm' class="btn" ></form><form method='POST' action='/toggle' enctype='multipart/form-data'> <label></label> <input type='submit' value='%TURN_ON_OFF%' class="btn btnLarge" ></form></div></div><div class="flex-card"><div class="hero"><h3>Settings</h3></div><div class="content"><h3>Network</h3><form method='POST' action='/' enctype='multipart/form-data'> <label for="netHost">Hostname</label> <input id="netHost" class="inputLarge" name="netHost" value="%HOSTNAME%"><br><h3>MQTT</h3> <label for="mqttHost">Host</label> <input id="mqttHost" class="inputMedium" name="mqttHost" value="%MQTT_HOST%"> : <input size="3" id="mqttPort" class="inputSmall" name="mqttPort" value="%MQTT_PORT%"><br><label for="mqttTopic">Topic</label> <input id="mqttTopic" class="inputLarge" name="mqttTopic" value="%MQTT_TOPIC%"><br><label for="mqttUsername">Username</label> <input id="mqttUsername" class="inputLarge" name="mqttUsername" value="%MQTT_USER%"><br><label for="mqttPassword">Password</label> <input id="mqttPassword" class="inputLarge" name="mqttPassword" value="%MQTT_PW%"><br> <br> <input type='submit' value='Update settings' class="btn btnLarge" ></form></div></div><div class="flex-card"><div class="hero"><h3>System</h3></div><div class="content"><h3>Firmware update</h3><form method='POST' action='/installUpdate' enctype='multipart/form-data'> <input type='file' class="input inputLarge" accept='.bin,.bin.gz' name='firmware' > <input type='submit' value='Update' class="btn" ></form> <br></div></div><div class="break"></div><div class="flex-card"><div class="hero"><h3>Log</h3></div><div class="content" id="logDiv"></div></div></div></body></html>)";
 
 static const char HTML_INSTALLED[] PROGMEM
   = R"(<html><style>html{background-color:#424242;font-size:16px;font-family:'Roboto',sans-serif;font-weight:300;color:#4a4a4a;color:#fefefe;text-align:center}</style><head><meta http-equiv="refresh" content="15;/" /></head><body><h1>Update %s, reloading in 15 seconds...</h1></body></html>)";
@@ -21,7 +21,6 @@ static const char HTML_REDIRECT_NOW[] PROGMEM
 
 String logBuffer_;
 
-
 void open_heat::network::WebServer::setup()
 {
   auto& config = filesystem_.getConfig();
@@ -30,13 +29,13 @@ void open_heat::network::WebServer::setup()
 
   const char* rootPath = "/";
   const char* installUpdatePath = "/installUpdate";
-  const char* offPath = "/off";
+  const char* togglePath = "/toggle";
 
-  asyncWebServer_.on(offPath, HTTP_POST, [this](AsyncWebServerRequest* request) {
-    offHandlePost(request);
+  asyncWebServer_.on(togglePath, HTTP_POST, [this](AsyncWebServerRequest* request) {
+    togglePost(request);
   });
 
-  asyncWebServer_.on(offPath, HTTP_GET, [this](AsyncWebServerRequest* request) {
+  asyncWebServer_.on(togglePath, HTTP_GET, [this](AsyncWebServerRequest* request) {
     request->send(HTTP_OK, CONTENT_TYPE_HTML, HTML_REDIRECT_NOW);
   });
 
@@ -243,10 +242,14 @@ bool WebServer::updateField(
   return true;
 }
 
-void WebServer::offHandlePost(AsyncWebServerRequest* request)
+void WebServer::togglePost(AsyncWebServerRequest* pRequest)
 {
-  valve_.setConfiguredTemp(0);
-  request->send(HTTP_OK, CONTENT_TYPE_HTML, HTML_REDIRECT_NOW);
+  auto newMode = valve_.getMode() == heating::RadiatorValve::Mode::OFF
+    ? heating::RadiatorValve::Mode::HEAT
+    : heating::RadiatorValve::Mode::OFF;
+  valve_.setMode(newMode);
+
+  pRequest->send(HTTP_OK, CONTENT_TYPE_HTML, HTML_REDIRECT_NOW);
 }
 
 String WebServer::indexHTMLProcessor(const String& var)
@@ -277,6 +280,15 @@ String WebServer::indexHTMLProcessor(const String& var)
   // Header
   else if (var == F("HOSTNAME")) {
     return config.Hostname;
+  }
+
+  // Mode
+  else if (var == F("TURN_ON_OFF")) {
+    if (valve_.getMode() == heating::RadiatorValve::HEAT) {
+      return F("Turn off");
+    } else {
+      return F("Turn on");
+    }
   }
 
   Logger::log(Logger::WARNING, "Invalid template: %s", var.c_str());
